548d07f337f9e0ae55976259768f22d052eb623e
Kevin Doolaeghe <doolaeghe.kevin@gmail.com>
2022-09-04 10:29:45 +0200

    added card-list and adapted content to navbar

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+/* cards */
+
+const cardGrid = document.querySelector(".card-grid");
+const cards = document.querySelectorAll(".card");
+
+const loadContent = async () => {
+    const result = await fetch("https://picsum.photos/v2/list");
+    const data = await result.json();
+    createCards(data);
+}
+loadContent();
+
+const createCards = data => {
+    let cardList = '';
+    for (let i = 0; i < cards.length; i++) {
+        const newCard = `
+            <div class="card">
+                <img src=${data[i].download_url} class="top-img" />
+                <div class="content">
+                    <h3 class="card-title">Lorem ipsum dolor sit amet</h3>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <p class="card-text">Lorem ipsum dolor</p>
+                </div>
+            </div>
+        `;
+
+        cardList += newCard;
+    }
+    cardGrid.innerHTML = cardList;
+}
+
+/* navbar */
+
 const hamburgerToggler = document.querySelector(".hamburger");
 const navLinksContainer = document.querySelector(".navlinks-container");
 
